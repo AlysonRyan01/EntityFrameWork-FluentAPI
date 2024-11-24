a838c8ffba8b5eed308d0406c40ad67f6b33e7ff
AlysonRyan01 <sdhuahdsaih@gmail.com>
2024-11-24 18:12:55 -0300

    .

---
+++ Data/Mappings/CategoryMap.cs
@@ -0,0 +1,15 @@
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Blog.Data.Mappings
+{
+    public class CategoryMap : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("Category");
+            builder.HasKey(x=> x.Id);
+        }
+    }
+}
